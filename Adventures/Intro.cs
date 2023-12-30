fdf1e846af244d85baa5dab885e0240ba8551579
Adeyemi-theRustyDev <abiadeabdulazeez@gmail.com>
2023-12-30 15:24:41 +0100

    Uno; Converted to dotnet-8 :)

---
+++ Adventures/Intro.cs
@@ -0,0 +1,23 @@
+﻿namespace Uno
+{
+	public class Intro
+	{
+		public static void Quests(string[] args)
+		{
+			Console.WriteLine("Which Quest do you want?");
+			string choice = Console.ReadLine()!;
+			string response;
+
+			response = choice switch
+			{
+				"1" => "Ye rest and recover your health",
+				"2" => "Raiding the port town get ye 50 gold doubloons",
+				"3" => "The wind is at your back; the open horizon ahead",
+				"4" => "T'is but a baby Kraken, but still it eats toy boats",
+				_ => "Apologies. I don't know that one."
+			};
+
+			Console.WriteLine(response);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Uno;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Person> people = new ()
+        {
+            new () { Name = "Adeyemi", DateOfBirth = new DateTime(2005, 02, 09) },
+            new () { Name = "Mariam",  DateOfBirth = new DateTime(2005, 05, 25) }
+        };
+        foreach (var person in people)
+        {
+            Console.WriteLine(person.Name);
+        }
+    }
+}
+
+internal class Person
+{
+    public string? Name { get; set; }
+    public DateTime DateOfBirth { get; set; }
+}
